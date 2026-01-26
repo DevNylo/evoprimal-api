9349725f58db0af55f0509f7d740cb0bbccd5c1f
Danilo Rocha <contato_dsr@hotmail.com>
2026-01-25 21:52:57 -0300

    Configura Cloudinary e instala Postgres

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,36 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  
+  /* --- INÍCIO DA ALTERAÇÃO DE SEGURANÇA --- */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com', // <--- AQUI: Liberamos o Cloudinary
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com', // <--- AQUI: Liberamos vídeos do Cloudinary
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* --- FIM DA ALTERAÇÃO --- */
+
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
